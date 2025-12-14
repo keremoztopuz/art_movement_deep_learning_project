a62a97ae0a1023ec369f96c6667961906e3a7a0e
keremozt <keremoztopuzzz@gmail.com>
2025-12-14 20:19:06 +0300

    train test split done

---
+++ src/dataset.py
@@ -0,0 +1,76 @@
+#data loading and augmentation
+
+import os
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from sklearn.model_selection import train_test_split
+from config import DATA_DIR, IMAGE_SIZE, MEAN, STD, BATCH_SIZE
+
+images = []
+labels = []
+
+train_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(MEAN, STD),
+])
+
+val_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(MEAN, STD),
+])
+
+class ArtDataset(Dataset):
+    def __init__(self, image_paths, labels, transform=None):
+        self.image_paths = image_paths
+        self.labels = labels
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.image_paths)
+    
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert("RGB")
+        label = self.labels[idx]
+
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+for folder in os.listdir(DATA_DIR):
+    folder_path = os.path.join(DATA_DIR, folder)
+    if os.path.isdir(folder_path):
+        for file in os.listdir(folder_path):
+            if file.endswith(".jpg"):
+                images.append(os.path.join(folder_path, file))
+                labels.append(folder)      
+
+classes = sorted(list(set(labels)))
+class_to_idx = {cls: idx for idx, cls in enumerate(classes)}
+labels = [class_to_idx[label] for label in labels]  
+
+X_train, X_temp, y_train, y_temp = train_test_split(
+    images,
+    labels,
+    stratify=labels,
+    test_size=0.3,
+    random_state=42,
+)
+    
+X_val, X_test, y_val, y_test = train_test_split(
+    X_temp,
+    y_temp,
+    stratify=y_temp,
+    test_size=0.5,
+    random_state=42,
+)    
+
+train_dataset = ArtDataset(X_train, y_train, transform=train_transform)
+val_dataset = ArtDataset(X_val, y_val, transform=val_transform)
+test_dataset = ArtDataset(X_test, y_test, transform=val_transform)
+
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
