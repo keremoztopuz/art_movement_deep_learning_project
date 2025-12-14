ae4398a882b782672f6ba98f39500b9b695eabf5
keremozt <keremoztopuzzz@gmail.com>
2025-12-14 20:19:14 +0300

    created

---
+++ src/model.py
@@ -0,0 +1,13 @@
+#model presentation
+
+import timm
+from config import MODEL_NAME, NUM_CLASSES
+
+def create_model():
+    model = timm.create_model(
+        MODEL_NAME, 
+        num_classes=NUM_CLASSES, 
+        pretrained=True
+    )
+    
+    return model
