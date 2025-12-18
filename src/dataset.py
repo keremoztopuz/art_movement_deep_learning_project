51ce5d661dbbfa504a26a706bbc43fabecc2e704
keremozt <keremoztopuzzz@gmail.com>
2025-12-18 18:36:20 +0300

    resize added, num_workers added and augmentation increased

--- src/dataset.py
+++ src/dataset.py
@@ -11,14 +11,17 @@ images = []
 labels = []
 
 train_transform = transforms.Compose([
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(15),
-    transforms.ColorJitter(brightness=0.2, contrast=0.2),
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(20),
+    transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.2, hue=0.1),
+    transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)),
     transforms.ToTensor(),
     transforms.Normalize(MEAN, STD),
 ])
 
 val_transform = transforms.Compose([
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
     transforms.ToTensor(),
     transforms.Normalize(MEAN, STD),
 ])
@@ -36,7 +39,6 @@ class ArtDataset(Dataset):
         img_path = self.image_paths[idx]
         image = Image.open(img_path).convert("RGB")
         label = self.labels[idx]
-
         if self.transform:
             image = self.transform(image)
         return image, label
@@ -47,32 +49,19 @@ for folder in os.listdir(DATA_DIR):
         for file in os.listdir(folder_path):
             if file.endswith(".jpg"):
                 images.append(os.path.join(folder_path, file))
-                labels.append(folder)      
+                labels.append(folder)
 
 classes = sorted(list(set(labels)))
 class_to_idx = {cls: idx for idx, cls in enumerate(classes)}
-labels = [class_to_idx[label] for label in labels]  
+labels = [class_to_idx[label] for label in labels]
 
-X_train, X_temp, y_train, y_temp = train_test_split(
-    images,
-    labels,
-    stratify=labels,
-    test_size=0.3,
-    random_state=42,
-)
-    
-X_val, X_test, y_val, y_test = train_test_split(
-    X_temp,
-    y_temp,
-    stratify=y_temp,
-    test_size=0.5,
-    random_state=42,
-)    
+X_train, X_temp, y_train, y_temp = train_test_split(images, labels, stratify=labels, test_size=0.3, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, stratify=y_temp, test_size=0.5, random_state=42)
 
 train_dataset = ArtDataset(X_train, y_train, transform=train_transform)
 val_dataset = ArtDataset(X_val, y_val, transform=val_transform)
 test_dataset = ArtDataset(X_test, y_test, transform=val_transform)
 
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2, pin_memory=True)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2, pin_memory=True)
